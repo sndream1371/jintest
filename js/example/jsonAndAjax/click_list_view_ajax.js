f338cfb97f0ba17ac0e3751b16d58e83a75b7289
sndream1371 <sndream1371@gmail.com>
2018-09-04 13:21:40 +0900

    4
    4

--- js/example/jsonAndAjax/click_list_view_ajax.js
+++ js/example/jsonAndAjax/click_list_view_ajax.js
@@ -1,12 +1,13 @@
 window.onload = function(){ //모든 HTML을 로드한뒤에 함수를 호출한다.
-	
+
 	var pageCounter = 1;
 	var animalContainer = document.getElementById("animal-info");
 	var btn = document.getElementById("btn");
 
 	btn.addEventListener("click", function() {
 	  var ourRequest = new XMLHttpRequest();
-	  ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-' + pageCounter + '.json');
+	  //ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-' + pageCounter + '.json'); //소스 개발자의 샘플 json주소
+		ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-' + pageCounter + '.json');
 	  ourRequest.onload = function() {
 		if (ourRequest.status >= 200 && ourRequest.status < 400) {
 		  var ourData = JSON.parse(ourRequest.responseText);
@@ -14,7 +15,7 @@ window.onload = function(){ //모든 HTML을 로드한뒤에 함수를 호출한
 		} else {
 		  console.log("We connected to the server, but it returned an error.");
 		}
-		
+
 	  };
 
 	  ourRequest.onerror = function() {
@@ -33,7 +34,7 @@ window.onload = function(){ //모든 HTML을 로드한뒤에 함수를 호출한
 
 	  for (i = 0; i < data.length; i++) {
 		htmlString += "<p>" + data[i].name + " is a " + data[i].species + " that likes to eat ";
-		
+
 		for (ii = 0; ii < data[i].foods.likes.length; ii++) {
 		  if (ii == 0) {
 			htmlString += data[i].foods.likes[ii];
@@ -58,4 +59,4 @@ window.onload = function(){ //모든 HTML을 로드한뒤에 함수를 호출한
 
 	  animalContainer.insertAdjacentHTML('beforeend', htmlString);
 	}
-}	
+}
